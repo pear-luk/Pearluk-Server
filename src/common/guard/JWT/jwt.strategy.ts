1fc03dde35daaa780c36570040f622a49b5018d4
Denovo <vo0v0000@naver.com>
2023-01-23 22:39:04 +0900

    [FEAT]#19 JWT guard

--- src/common/guard/JWT/jwt.strategy.ts
+++ src/common/guard/JWT/jwt.strategy.ts
@@ -8,7 +8,7 @@ import { UserRopository } from './../../../user/provider/user.ropository';
 
 import { JwtPayload } from './jtw.payload';
 @Injectable()
-export class JwtAccessStrategyUser extends PassportStrategy(
+export class JwtAccessStrategy extends PassportStrategy(
   Strategy,
   'JWT-ACCESS-TOKEN',
 ) {
@@ -26,6 +26,7 @@ export class JwtAccessStrategyUser extends PassportStrategy(
   }
 
   async validate(payLoad: JwtPayload) {
+    console.log(payLoad);
     const { user_id } = payLoad;
     const user = await this.userRepo.checkUserJWT({ user_id });
 
