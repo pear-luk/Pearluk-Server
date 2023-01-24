be4207369bd862c32138c152844a36427745689a
Denovo <vo0v0000@naver.com>
2023-01-24 19:50:10 +0900

    [FEAT]#22 archive관련 API doc (swagger)

--- src/archive/archive.controller.ts
+++ src/archive/archive.controller.ts
@@ -7,31 +7,119 @@ import {
   Put,
   UseGuards,
 } from '@nestjs/common';
-import { CurrentUser } from '../common/decorator/current-user.decorator';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AdminAuthGuard } from './../common/guard/adminGuard';
 import { JwtAccessAuthGuard } from './../common/guard/JWT/jwt.guard';
 import { BaseResponse } from './../common/util/res/BaseResponse';
 import { baseResponeStatus } from './../common/util/res/baseStatusResponse';
-import { ArchiveCreateInputDTO } from './dto/create_archive.dto';
-import { ArchiveUpdateInputDTO } from './dto/update_archive.dto';
+import {
+  ArchiveCreateInputDTO,
+  ArchiveCreateResponseEX,
+} from './dto/create_archive.dto';
+import {
+  ArchiveDeleteResponseEX,
+  ArchiveUpdateInputDTO,
+  ArchiveUpdateResponseEX,
+} from './dto/update_archive.dto';
 
 import { ArchiveService } from './provider/archive.service';
 
+@ApiTags('Archive API')
 @Controller('archives')
 export class ArchiveController {
   constructor(private readonly archiveService: ArchiveService) {}
 
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: '아카이브 생성 API',
+    description: `
+      아카이브 생성 API입니다.
+      관리자만 접근이 가능합니다.
+
+      필요한 정보
+      - title : 아카이브 이름 String
+      - year : 아카이브 해당 년도 Int
+      - introduce ?: 아카이브 소개 String 
+    `,
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'SUCCESS',
+    schema: {
+      example: new BaseResponse(
+        baseResponeStatus.SUCCESS,
+        ArchiveCreateResponseEX,
+      ),
+    },
+  })
+  @ApiBadRequestResponse({
+    description: '이미 존재하는 아카이브 이름',
+    schema: {
+      example: baseResponeStatus.ARCHIVE_EXIST,
+    },
+  })
   @Post('/')
   @UseGuards(JwtAccessAuthGuard, AdminAuthGuard)
-  async creatArchive(
-    @Body() archiveInputDTO: ArchiveCreateInputDTO,
-    @CurrentUser() user,
-  ) {
+  async creatArchive(@Body() archiveInputDTO: ArchiveCreateInputDTO) {
     const result = await this.archiveService.createArchive(archiveInputDTO);
 
     return new BaseResponse(baseResponeStatus.SUCCESS, result);
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: '아카이브 수정 API',
+    description: `
+      아카이브 업데이트 API입니다.
+      관리자만 접근이 가능합니다.
+    
+      params
+      archive_id : 아카이브 id String (ULID)
+    
+      업데이트할 정보만 입력합니다.
+      - title ?: 아카이브 이름 String
+      - year ?: 아카이브 해당 년도 Int (2000~현재년도+1)
+      - introduce ?: 아카이브 소개 String 
+
+    `,
+  })
+  @ApiParam({
+    name: 'archive_id',
+    description: '아카이브 id - String ULID',
+    type: 'string',
+    example: ArchiveUpdateResponseEX.archive_id,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'SUCCESS',
+    schema: {
+      example: new BaseResponse(
+        baseResponeStatus.SUCCESS,
+        ArchiveUpdateResponseEX,
+      ),
+    },
+  })
+  @ApiResponse({
+    status: 400.2,
+    description: '이미 존재하는 타이틀',
+    schema: {
+      example: baseResponeStatus.ARCHIVE_EXIST,
+    },
+  })
+  @ApiResponse({
+    status: 400.1,
+    description: '존재하지 않는 아카이브',
+    schema: {
+      example: baseResponeStatus.ARCHIVE_NOT_EXIST,
+    },
+  })
   @Patch('/:archive_id')
   @UseGuards(JwtAccessAuthGuard, AdminAuthGuard)
   async updateArchive(
@@ -47,6 +135,41 @@ export class ArchiveController {
     return new BaseResponse(baseResponeStatus.SUCCESS, result);
   }
 
+  @ApiBearerAuth()
+  @ApiOperation({
+    summary: '아카이브 삭제 API',
+    description: `
+      아카이브 삭제 API입니다.
+      관리자만 접근이 가능합니다.
+    
+      params
+      archive_id : 아카이브 id String (ULID)
+    
+    `,
+  })
+  @ApiParam({
+    name: 'archive_id',
+    description: '아카이브 id - String ULID',
+    type: 'string',
+    example: ArchiveUpdateResponseEX.archive_id,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'SUCCESS',
+    schema: {
+      example: new BaseResponse(
+        baseResponeStatus.SUCCESS,
+        ArchiveDeleteResponseEX,
+      ),
+    },
+  })
+  @ApiResponse({
+    status: 400.1,
+    description: '존재하지 않는 아카이브',
+    schema: {
+      example: baseResponeStatus.ARCHIVE_NOT_EXIST,
+    },
+  })
   @Put('/:archive_id')
   @UseGuards(JwtAccessAuthGuard, AdminAuthGuard)
   async deleteStatusArchive(@Param('archive_id') archive_id) {
--- src/archive/dto/create_archive.dto.ts
+++ src/archive/dto/create_archive.dto.ts
@@ -1,17 +1,42 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Prisma } from '@prisma/client';
 import { IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
 export class ArchiveCreateInputDTO
   implements Omit<Prisma.ArchiveCreateInput, 'archive_id'>
 {
+  @ApiProperty({
+    name: 'title',
+    description: '아카이브 이름',
+  })
   @IsString()
   title: string;
 
+  @ApiProperty({
+    name: 'year',
+    description: '아카이브 해당 년도',
+  })
   @IsInt()
   @Min(2000)
   @Max(new Date().getFullYear() + 1)
   year: number;
 
+  @ApiProperty({
+    name: 'introduce',
+    description: '아카이브 설명',
+  })
   @IsOptional()
   @IsString()
   introduce?: string;
 }
+export class ArchiveCreateResponseDTO {
+  archive_id: string;
+  title: string;
+  year: number;
+  introduce?: string;
+}
+export const ArchiveCreateResponseEX = {
+  archive_id: '01GQFWTFS8XMHCC2RZY4P7HZ84',
+  title: 'zl존흑기sr 컬렉션',
+  year: 2023,
+  introduce: '지존 흑기사의 악세사리 컬렉션',
+};
