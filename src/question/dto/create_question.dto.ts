55afe59b63bb2e9cd0429106653b88d427298968
yunseo323 <jokani3244833@gmail.com>
2023-02-02 16:22:55 +0900

    question type enum 추가

--- src/question/dto/create_question.dto.ts
+++ src/question/dto/create_question.dto.ts
@@ -1,7 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Prisma } from '@prisma/client';
-import { IsNumber } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsEnum, IsNumber } from 'class-validator';
 import { IsULID } from 'src/common/decorator/IsULID';
+import { E_QuestionType } from '../interface/question_type.enum';
 
 export class QuestionCreateInputDTO
   implements Omit<Prisma.QuestionCreateInput, 'question_id'>
@@ -10,9 +12,14 @@ export class QuestionCreateInputDTO
     name: 'type',
     description: '질문 유형',
     type: 'number',
-    //enum 추가
+    enum: E_QuestionType,
+    default: 0,
   })
   @IsNumber()
+  @IsEnum(E_QuestionType)
+  @Transform(({ value }) =>
+    !isNaN(Number(value)) ? Number(value) : E_QuestionType[value],
+  )
   @ApiProperty({
     name: 'product_id',
     description: '상품 id',
