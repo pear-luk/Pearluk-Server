6289b754c46bb43de3a1032b27cf3bb6ae4343b4
Denovo <vo0v0000@naver.com>
2023-02-03 18:28:35 +0900

    slack test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { CurrentUser } from './common/decorator/current-user.decorator';
-import { AdminAuthGuard } from './common/guard/adminGuard';
-import { JwtAccessAuthGuard } from './common/guard/JWT/jwt.guard';
 import { BaseResponse } from './common/util/res/BaseResponse';
 import { baseResponeStatus } from './common/util/res/baseStatusResponse';
 
@@ -11,8 +9,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  @UseGuards(JwtAccessAuthGuard, AdminAuthGuard)
+  @Get('/')
   getHello(@CurrentUser() user): BaseResponse<string> {
     const result = this.appService.getHello();
     console.log(user);
