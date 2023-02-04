76d808659340d49e80015dff736f246a71b7e723
yunseo323 <jokani3244833@gmail.com>
2023-02-05 00:04:41 +0900

    [FEAT]#33 quesion dto 수정

--- src/question/dto/update_question.dto.ts
+++ src/question/dto/update_question.dto.ts
@@ -8,12 +8,23 @@ export class QuestionUpdateInputDTO
   implements Prisma.QuestionUpdateInput
 {
   @IsOptional()
+  contents?: string;
+
+  @IsOptional()
   type?: number;
+
+  @IsOptional()
+  secret_mode?: number;
+
+  @IsOptional()
+  password?: string;
 
   @IsOptional()
   product_id?: string;
 }
 export const updateQuestionInputEX = {
+  contents: '질문 수정입니다ㅏㅏㅏ',
   type: 1,
+  secret_mode: 0,
   product_id: '123455555',
 };
