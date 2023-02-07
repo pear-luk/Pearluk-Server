0c06e10a73808c9b0e1f0a16b38d160957df5a69
Denovo <vo0v0000@naver.com>
2023-02-07 23:56:15 +0900

    [FEAT][DB]#2 #33 #30 1:n 관계 복수-> 단수로 수정, OrderProduct 쿠폰오타 수정, OrderProduct_Product 관계형성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,9 +4,12 @@ import { CurrentUser } from '../common/decorator/current-user.decorator';
 import { JwtAccessAuthGuard } from '../common/guard/JWT/jwt.guard';
 import { baseResponeStatus } from './../common/util/res/baseStatusResponse';
 import { Payload } from './dto/payload.dto';
+import { AuthService } from './provider/auth.service';
 
 @Controller('/auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   //로그인 상태 리턴.
   @Get('/')
   @UseGuards(JwtAccessAuthGuard)
