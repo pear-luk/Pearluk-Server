62a6a2e0e329edfa6266373bc72a1108706179ce
yunseo323 <jokani3244833@gmail.com>
2023-02-10 22:30:00 +0900

    [FEAT]#33 secret_mode 추가 & Question Update 구현

--- src/question/dto/update_question.dto.ts
+++ src/question/dto/update_question.dto.ts
@@ -8,6 +8,9 @@ export class QuestionUpdateInputDTO
   implements Prisma.QuestionUpdateInput
 {
   @IsOptional()
+  title?: string;
+
+  @IsOptional()
   contents?: string;
 
   @IsOptional()
@@ -23,8 +26,8 @@ export class QuestionUpdateInputDTO
   product_id?: string;
 }
 export const updateQuestionInputEX = {
+  title: '질문수정1',
   contents: '질문 수정입니다ㅏㅏㅏ',
   type: 1,
-  secret_mode: 0,
-  product_id: '123455555',
+  secret_mode: 0, //공개
 };
--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiBearerAuth,
@@ -11,6 +19,7 @@ import {
 } from '@nestjs/swagger';
 import { AdminAuthGuard } from 'src/common/guard/adminGuard';
 import { JwtAccessAuthGuard } from 'src/common/guard/JWT/jwt.guard';
+import { DevGuard } from '../common/guard/devGuard';
 import { BaseResponse } from './../common/util/res/BaseResponse';
 import { baseResponeStatus } from './../common/util/res/baseStatusResponse';
 import {
@@ -28,19 +37,35 @@ export class QuestionController {
   constructor(private readonly questionService: QuestionService) {}
 
   @Get('/qa') //질문리스트 조회
+  @UseGuards(DevGuard)
   async getQuestionList() {
     const result = '성공';
     return result;
   }
 
   @Post('/qa') //질문하기
+  @UseGuards(DevGuard)
   async createQuestion(@Body() questionInputDTO: QuestionCreateInputDTO) {
     const result = await this.questionService.createQuestion(questionInputDTO);
     return new BaseResponse(baseResponeStatus.SUCCESS, result);
   }
 
-  @Get()
-  async getQA() {
-    return 'getQA!';
+  @Patch('/:question_id')
+  @UseGuards(DevGuard)
+  async updateQuestion(
+    @Param('question_id') question_id: string,
+    @Body() QuestionUpdateInputDTO: QuestionUpdateInputDTO,
+  ) {
+    const result = await this.questionService.updateQuestion({
+      question_id,
+      ...QuestionUpdateInputDTO,
+    });
+    return new BaseResponse(baseResponeStatus.SUCCESS, result);
   }
+
+  // @Get()
+  // @UseGuards(DevGuard)
+  // async getQA() {
+  //   return 'getQA!';
+  // }
 }
