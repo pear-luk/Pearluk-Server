38fcb3516034d503de3dcb5c47d83d20cd6d502a
yunseo323 <jokani3244833@gmail.com>
2023-02-12 21:18:24 +0900

    question더미데이터 생성 구현

--- src/question/question.module.ts
+++ src/question/question.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
-import { QuestionController } from './question.controller';
-import { QuestionService } from './provider/question.service';
 import { ProductModule } from 'src/product/product.module';
-import { QuestionRepository } from './provider/question.repository';
 import { UserModule } from '../user/user.module';
+import { QuestionFaker } from './provider/question.faker';
+import { QuestionRepository } from './provider/question.repository';
+import { QuestionService } from './provider/question.service';
+import { QuestionController } from './question.controller';
 
 @Module({
   imports: [ProductModule, UserModule],
   controllers: [QuestionController],
-  providers: [QuestionService, QuestionRepository],
+  providers: [QuestionService, QuestionRepository, QuestionFaker],
   exports: [],
 })
 export class QuestionModule {}
